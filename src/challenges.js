f1ad4b2f29848f1dd115b28e423d92b9cbc24522
Gabriel Oliva <gfpoliva@gmail.com>
2021-07-02 12:59:41 -0300

    Disponibilização do projeto

---
+++ src/challenges.js
@@ -0,0 +1,41 @@
+// 1.Quanto custa o sanduíche trybe?
+// 2.Montar e validar sanduíche recebendo ingredientes e retornar o preço 
+// 3.Calcular menu com promoção baseado no dia
+// 4.Procurar pizzas mais baratas que X
+// 5.Calcular preço final e quanto fica pra cada amigo
+// 6.Retornar lanches e preço dos combos
+const data = require('../data');
+
+function getSandwichPrice(sandwichName) {
+
+}
+
+function getDayMenu(day) {
+
+}
+
+function getSharedBill(ordersIDs, qtd) {
+
+}
+
+function makeSandwich(items) {
+
+}
+
+function getCheapestPizza(price) {
+
+}
+
+function getComboSandwichsAndPrices() {
+
+}
+
+module.exports = {
+  getSandwichPrice,
+  getDayMenu,
+  getSharedBill,
+  makeSandwich,
+  getCheapestPizza,
+  getComboSandwichsAndPrices
+}
+
